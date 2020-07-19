5143da5822d58e66db5bfea9ce4567ef272b8c0e
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-07-20 00:39:27 +0100

    tf-idf analysis

---
+++ R/05-compare.R
@@ -0,0 +1,57 @@
+# load packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(tidytext)
+library(topicmodels)
+library(here)
+
+# set theme for plots to minimal -----------------------------------------------
+
+theme_set(theme_minimal())
+
+# set color --------------------------------------------------------------------
+
+scotblue <- "#0065BF"
+ukred <- "#D00C27"
+
+# read data --------------------------------------------------------------------
+
+covid_speeches_scot_words <- read_rds(here::here("processed-data", "covid_speeches_scot_words.rds"))
+covid_speeches_scot_words <- covid_speeches_scot_words %>%
+  mutate(origin = "Scotland")
+
+covid_speeches_uk_words <- read_rds(here::here("processed-data", "covid_speeches_uk_words.rds"))
+covid_speeches_uk_words <- covid_speeches_uk_words %>%
+  mutate(origin = "UK")
+
+# bind data& calculate tf-idf --------------------------------------------------
+
+covid_speeches_words <- bind_rows(covid_speeches_scot_words, covid_speeches_uk_words) %>%
+  count(origin, word, sort = TRUE) %>%
+  group_by(origin) %>%
+  mutate(total = sum(n)) %>%
+  bind_tf_idf(word, origin, n)
+
+# terms with high tf-idf -------------------------------------------------------
+
+covid_speeches_words %>%
+  select(-total) %>%
+  arrange(desc(tf_idf))
+
+# visualise tf-idf -------------------------------------------------------------
+
+covid_speeches_words %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(origin) %>% 
+  top_n(15) %>% 
+  ungroup() %>%
+  ggplot(aes(y = word, x = tf_idf, fill = origin)) +
+  geom_col(show.legend = FALSE) +
+  labs(y = NULL, x = "tf-idf") +
+  facet_wrap(~origin, ncol = 2, scales = "free") +
+  scale_fill_manual(values = c(scotblue, ukred))
+
+# save data --------------------------------------------------------------------
+
+write_rds(covid_speeches_words, "processed-data/covid_speeches_words.rds")
