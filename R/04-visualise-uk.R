c3afdd52b252e0d171f97711f307aaba18eb5c04
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-11-29 20:41:12 +0000

    Old UK data from rev.com to move into a branch

--- R/04-visualise-uk.R
+++ R/04-visualise-uk.R
@@ -21,6 +21,32 @@ ukred <- "#D00C27"
 covid_speeches_uk <- read_rds(here::here("data", "covid-speeches-uk.rds")) %>%
   rowid_to_column(var = "speech_no")
 
+# speakers ---------------------------------------------------------------------
+
+speakers <- covid_speeches_uk %>%
+  unnest_longer(text) %>%
+  group_by(speech_no) %>%
+  slice_head(n = 1) %>%
+  ungroup() %>%
+  mutate(speaker = text %>% str_match(".*?:") %>% str_remove(":")) %>%
+  select(speech_no, speaker)
+
+# remove paragraphs not spoken by main speaker ---------------------------------
+
+covid_speeches_uk <- covid_speeches_uk %>%
+  left_join(speakers, by = "speech_no") %>%
+  unnest_longer(text) %>%
+  mutate(
+    speaker = text %>% str_match(".*?:") %>% str_remove(":"),
+    text    = str_remove(text, ".*?\\) ")
+    ) %>%
+  group_by(speech_no) %>%
+  mutate(first_speaker = cumall(speaker == lag(speaker, default = speaker[1]))) %>%
+  filter(first_speaker) %>%
+  select(-first_speaker) %>%
+  nest(text = c(text)) %>%
+  mutate(text = map(text, ~.[[1]]))
+
 # determine day of week --------------------------------------------------------
 
 covid_speeches_uk <- covid_speeches_uk %>%
@@ -35,12 +61,24 @@ covid_speeches_uk <- covid_speeches_uk %>%
 
 covid_speeches_uk <- covid_speeches_uk %>%
   rowwise() %>%
-  mutate(n_words = text %>% str_count("\\w+") %>% sum()) %>%
+  mutate(
+    n_paragraphs = unlist(text) %>% length(),
+    n_words      = unlist(text) %>% str_count("\\w+") %>% sum()
+  ) %>%
   ungroup()
 
 # how long do they speak? ------------------------------------------------------
 
-# number of words ----
+# number of paragraphs ----
+
+ggplot(covid_speeches_uk, aes(x = n_paragraphs)) +
+  geom_density(color = ukred, fill = ukred, alpha = 0.5) +
+  labs(
+    title = "Distribution of number of paragphs",
+    subtitle = "of UK daily briefings",
+    x = "Number of paragraphs",
+    y = "Density"
+  )
 
 ggplot(covid_speeches_uk, aes(x = n_words)) +
   geom_density(color = ukred, fill = ukred, alpha = 0.5) +
@@ -49,6 +87,22 @@ ggplot(covid_speeches_uk, aes(x = n_words)) +
     subtitle = "of UK daily briefings",
     x = "Number of words",
     y = "Density"
+  )
+
+# number of paragraphs vs. date ----
+
+lm_paragraphs <- lm(n_paragraphs ~ date, data = covid_speeches_uk)
+lm_paragraphs_rsq <- glance(lm_paragraphs)$r.squared
+
+covid_speeches_uk %>%
+  ggplot(aes(x = date, y = n_paragraphs)) +
+  geom_point(color = ukred, alpha = 0.7) +
+  geom_smooth(aes(x = date, y = n_paragraphs), method = lm, formula = y ~ x, color = "darkgray") +
+  scale_color_viridis_d(begin = 0.4, end = 0.9) +
+  labs(
+    title = "Length of UK COVID-19 speeches",
+    subtitle = glue("Measured in number of paragraphs, R-squared = {percent(lm_paragraphs_rsq)}"),
+    x = NULL, y = "Number of paragraphs", color = NULL, shape = NULL
   )
 
 # number of words vs. date ----
@@ -69,17 +123,27 @@ covid_speeches_uk %>%
 
 # word analysis ----------------------------------------------------------------
 
-covid_speeches_uk_words <- covid_speeches_uk %>%
+covid_speeches_uk_paragraphs <- covid_speeches_uk %>%
+  unnest_longer(
+    col = text,
+    values_to = "paragraph",
+    indices_to = "paragraph_no"
+    ) %>%
+  group_by(speech_no) %>%
+  mutate(paragraph_no = row_number()) %>%
+  ungroup()
+
+covid_speeches_uk_words <- covid_speeches_uk_paragraphs %>%
   # make sure COVID-19 (and all its various spellings) don't get split
   # tidytext doesn't remove underscores
   # https://stackoverflow.com/questions/58281091/preserve-hyphenated-words-in-ngrams-analysis-with-tidytext
   mutate(
-    text = str_replace_all(text, "COVID-19", "COVID_19"),
-    text = str_replace_all(text, "COVID 19", "COVID_19"),
-    text = str_replace_all(text, "Covid-19", "COVID_19"),
-    text = str_replace_all(text, "Covid 19", "COVID_19")
+    paragraph = str_replace_all(paragraph, "COVID-19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "COVID 19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "Covid-19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "Covid 19", "COVID_19")
   ) %>%
-  unnest_tokens(word, text) %>%
+  unnest_tokens(word, paragraph) %>%
   anti_join(stop_words) %>%
   filter(!(word %in% as.character(paste0(0, 0:9))))
 
@@ -179,24 +243,24 @@ covid_speeches_uk_words %>%
 
 # 2-grams ----------------------------------------------------------------------
 
-covid_speeches_uk_bigrams <- covid_speeches_uk %>%
+covid_speeches_uk_bigrams <- covid_speeches_uk_paragraphs %>%
   # make sure COVID-19 (and all its various spellings) don't get split
   # tidytext doesn't remove underscores
   # https://stackoverflow.com/questions/58281091/preserve-hyphenated-words-in-ngrams-analysis-with-tidytext
   mutate(
-    text = str_replace_all(text, "COVID-19", "COVID_19"),
-    text = str_replace_all(text, "COVID 19", "COVID_19"),
-    text = str_replace_all(text, "Covid-19", "COVID_19"),
-    text = str_replace_all(text, "Covid 19", "COVID_19")
+    paragraph = str_replace_all(paragraph, "COVID-19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "COVID 19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "Covid-19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "Covid 19", "COVID_19")
   ) %>%
-  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+  unnest_tokens(bigram, paragraph, token = "ngrams", n = 2) %>%
   # drop bigrams with stopwords
   mutate(i = row_number()) %>%    # add index for later grouping
   unnest_tokens(word, bigram, drop = FALSE) %>%    # tokenize bigrams into words
   anti_join(stop_words) %>%    # drop rows with stop words
   group_by(i) %>%    # group by bigram index
   filter(n() == 2) %>%    # drop bigram instances where only one word left
-  summarise(bigram = unique(bigram), .groups = "drop")
+  summarise(bigram = unique(bigram))
 
 covid_speeches_uk_bigrams %>%
   mutate(
@@ -210,24 +274,24 @@ covid_speeches_uk_bigrams %>%
   guides(fill = FALSE) +
   labs(
     title = "Frequency of bigrams in UK COVID-19 briefings",
-    subtitle = "Bigrams occurring more than 15 times",
+    subtitle = "Bigrams occurring more than 20 times",
     y = NULL, x = NULL
   )
 
 # social to physical distancing ------------------------------------------------
 # they never say physical distancing!
 
-covid_speeches_uk %>%
+covid_speeches_uk_paragraphs %>%
   # make sure COVID-19 (and all its various spellings) don't get split
   # tidytext doesn't remove underscores
   # https://stackoverflow.com/questions/58281091/preserve-hyphenated-words-in-ngrams-analysis-with-tidytext
   mutate(
-    text = str_replace_all(text, "COVID-19", "COVID_19"),
-    text = str_replace_all(text, "COVID 19", "COVID_19"),
-    text = str_replace_all(text, "Covid-19", "COVID_19"),
-    text = str_replace_all(text, "Covid 19", "COVID_19")
+    paragraph = str_replace_all(paragraph, "COVID-19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "COVID 19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "Covid-19", "COVID_19"),
+    paragraph = str_replace_all(paragraph, "Covid 19", "COVID_19")
   ) %>%
-  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+  unnest_tokens(bigram, paragraph, token = "ngrams", n = 2) %>%
   filter(str_detect(bigram, "social dist|physical dist")) %>%
   mutate(soc_phys = if_else(str_detect(bigram, "social"), "S", "P")) %>%
   count(date, soc_phys) %>%
@@ -242,4 +306,5 @@ covid_speeches_uk %>%
 
 write_rds(covid_speeches_uk, file = "processed-data/covid_speeches_uk.rds")
 write_rds(covid_speeches_uk_bigrams, file = "processed-data/covid_speeches_uk_bigrams.rds")
+write_rds(covid_speeches_uk_paragraphs, file = "processed-data/covid_speeches_uk_paragraphs.rds")
 write_rds(covid_speeches_uk_words, file = "processed-data/covid_speeches_uk_words.rds")
